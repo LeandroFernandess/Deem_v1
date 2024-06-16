9de9a6f966e4f650343861223ae7708b281a99d4
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-06-16 19:08:41 -0300

    Update excel_file.py

--- export/excel_file.py
+++ export/excel_file.py
@@ -4,7 +4,7 @@ import io
 
 
 # Criando a função para exportar em Excel:
-def convert_df_to_excel(df):
+def ConvertExcel(df):
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine="openpyxl") as writer:
         df.to_excel(writer, index=False, sheet_name="Divergências")

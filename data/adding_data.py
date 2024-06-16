b99718764bc33e1c29c7f3ea2108b90edafb8602
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-06-16 19:09:27 -0300

    Update adding_data.py

--- data/adding_data.py
+++ data/adding_data.py
@@ -1,34 +1,40 @@
-# Importando as bibliotecas necessárias:
-
-
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
+from firebase_admin import credentials, firestore, storage
+from datetime import datetime
 
 # Inicializando o serviço do banco de dados Firebase:
-
 
 try:
     firebase_admin.get_app()
 except ValueError as e:
     cred = credentials.Certificate("credentials.json")
-    firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app(cred, {"storageBucket": "deem-fa6c8.appspot.com"})
 
 database = firestore.client()
 
 
-# Criando a função para adicionar as informações do formulário no banco de dados Firebase:
+def AddImage(uploaded_file, user_id):
+    bucket = storage.bucket()
+    blob = bucket.blob(
+        f"{user_id}/{datetime.now().strftime('%Y%m%d%H%M%S')}_{uploaded_file.name}"
+    )
+    blob.upload_from_string(uploaded_file.getvalue(), content_type=uploaded_file.type)
+    return blob.public_url
 
 
-def add_data(name, code, quantity, rc, type):
+def AddData(name, code, quantity, rc, area, observation, type, files, user_id):
+    file_urls = [AddImage(file, user_id) for file in files]
+
     doc_ref = database.collection("users").document()
     doc_ref.set(
         {
-            "name": name,
-            "code": code,
-            "quantity": quantity,
-            "rc": rc,
-            "type": type,
+            "Responsável": name,
+            "Código": code,
+            "Quantidade": quantity,
+            "RC": rc,
+            "Área": area,
+            "Observação": observation,
+            "Tipo": type,
+            "Imagem": file_urls,
         }
     )

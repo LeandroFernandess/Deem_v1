91b2a02e01d02b750f2772cd123602b2c92bf307
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-06-23 21:21:55 -0300

    Add files via upload

---
+++ information/info.py
@@ -0,0 +1,94 @@
+from data.getting_data import get_data
+import streamlit as st
+from login.login import Login
+from export.excel_file import ConvertExcel
+
+
+def Table():
+
+    # Criando o título do formulário:
+
+    st.markdown(
+        "<h1 style='text-align: center;'>Relatório de divergências</h1>",
+        unsafe_allow_html=True,
+    )
+    st.write("---")
+
+    # Verificando se o usuário está logado:
+
+    if "user_id" not in st.session_state:
+        user_id = Login()
+        if user_id:
+            st.session_state.user_id = user_id
+        else:
+            st.stop()
+    else:
+        user_id = st.session_state.user_id
+
+    # Obtendo os dados do usuário logado:
+
+    user_data = get_data(user_id)
+
+    # Mantendo os valores dos inputs no session state:
+
+    if "name" not in st.session_state:
+        st.session_state.name = user_data.get("name", "")
+
+    if "code" not in st.session_state:
+        st.session_state.code = user_data.get("code", "")
+
+    if "quantity" not in st.session_state:
+        st.session_state.quantity = user_data.get("quantity", "")
+
+    if "std" not in st.session_state:
+        price = user_data.get("std", 0)
+        st.session_state.std = (
+            f"R$ {price:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+        )
+
+    if "input_total" not in st.session_state:
+        st.session_state.input_total = ""
+
+    if "rc" not in st.session_state:
+        st.session_state.rc = user_data.get("rc", "")
+
+    if "observation" not in st.session_state:
+        st.session_state.observation = user_data.get("observation", "")
+
+    if "input_description" not in st.session_state:
+        st.session_state.input_description = user_data.get("description", "")
+
+    # Obter os dados:
+
+    Table = get_data()
+
+    # Selecionar e ordenar as colunas:
+
+    desired_columns = [
+        "Responsável",
+        "Código",
+        "Descrição",
+        "Quantidade",
+        "Valor Unitário",
+        "Valor Total",
+        "Tipo",
+        "RC",
+        "Observação",
+        "Data",
+    ]
+    Table = Table[desired_columns]
+
+    # Exibir o DataFrame:
+
+    st.dataframe(Table)
+
+    # Verificar se o DataFrame não está vazio e permitir download:
+
+    if not Table.empty:
+        excel_data = ConvertExcel(Table)
+        st.download_button(
+            label="Extrair para Excel",
+            data=excel_data,
+            file_name="Deem.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )

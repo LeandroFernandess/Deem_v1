2755de9672d757bea01e3aae565f25382d5bee2e
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-07-02 21:20:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from firebase_admin import firestore
 from inputs.form import FormDeem
 from streamlit_option_menu import option_menu
 from information.info import Table
+from login.login import Login
+from logout.logoff import logout
 
 # Inicializando o serviço do banco de dados Firebase:
-
 try:
     firebase_admin.get_app()
 except ValueError as e:
@@ -21,24 +22,41 @@ st.set_page_config(page_title="Controle de Deem")
 
 def app():
 
-    with st.sidebar:
-        option = option_menu(
-            menu_title="Menu de navegação",
-            options=[
-                "Formulário de Deem",
-                "Visão Geral",
-                "Recarregar",
-            ],
-            icons=["journal-text", "bar-chart-fill", "arrow-clockwise"],
-            default_index=0,
-        )
+    # Garante que sempre teremos um estado inicial de não logado:
 
-    if option == "Formulário de Deem":
-        FormDeem()
-    if option == "Visão Geral":
-        Table()
-    if option == "Recarregar":
-        pass
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+
+    # Verificamos se o usuário está logado:
+
+    if not st.session_state.logged_in:
+        user = Login()
+        if user:
+            st.session_state.logged_in = True
+            st.session_state.user_id = user
+            st.rerun()
+    else:
+        with st.sidebar:
+            option = option_menu(
+                menu_title="Menu de navegação",
+                options=["Formulário de Deem", "Visão Geral", "Recarregar", "Sair"],
+                icons=[
+                    "journal-text",
+                    "bar-chart-fill",
+                    "arrow-clockwise",
+                    "door-closed",
+                ],
+                default_index=0,
+            )
+
+        if option == "Formulário de Deem":
+            FormDeem()
+        if option == "Visão Geral":
+            Table()
+        if option == "Recarregar":
+            pass
+        if option == "Sair":
+            logout()
 
 
 if __name__ == "__main__":

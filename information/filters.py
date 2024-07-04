fc67dc26d728cee7f8016d6e91e76c0d4103b022
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-07-03 22:13:08 -0300

    Update filters.py

--- information/filters.py
+++ information/filters.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 def filters(Table):
 
+    # Criar duas colunas para os filtros:
+    col1, col2, col3 = st.columns(3)
+
     # Filtrar os dados por responsável:
-    
     responsible = Table["Responsável"].unique()
-    selected_responsible = st.multiselect(
-        "Filtrar por responsável",
+    selected_responsible = col1.multiselect(
+        "Filtrar por Responsável",
         responsible,
         placeholder="Escolha um responsável",
     )
@@ -16,16 +18,14 @@ def filters(Table):
         Table = Table[Table["Responsável"].isin(selected_responsible)]
 
     # Filtrar os dados por código:
-    
-    code_filter = st.text_input(
+    code_filter = col1.text_input(
         "Filtrar por código", value="", placeholder="Digite um código"
     )
     if code_filter:
         Table = Table[Table["Código"].str.contains(code_filter, case=False, na=False)]
 
     # Filtrar os dados por descrição:
-    
-    description_filter = st.text_input(
+    description_filter = col1.text_input(
         "Filtrar por descrição", value="", placeholder="Digite uma descrição"
     )
     if description_filter:
@@ -34,17 +34,15 @@ def filters(Table):
         ]
 
     # Filtrar os dados por tipo:
-    
-    type_filter = st.text_input(
+    type_filter = col2.text_input(
         "Filtrar por tipo", value="", placeholder="Digite o tipo da Deem"
     )
     if type_filter:
         Table = Table[Table["Tipo"].str.contains(type_filter, case=False, na=False)]
 
     # Filtrar os dados por RC:
-    
-    rc_filter = st.text_input(
-        "Filtrar por relação de rarga",
+    rc_filter = col2.text_input(
+        "Filtrar por Relação de Carga",
         value="",
         placeholder="Digite a Relação de Carga",
     )
@@ -52,15 +50,12 @@ def filters(Table):
         Table = Table[Table["RC"].str.contains(rc_filter, case=False, na=False)]
 
     # Converter a coluna de data para datetime:
-    
     Table = Table.copy()  # Adicionado para evitar o aviso de SettingWithCopyWarning
-    
     Table.loc[:, "Data"] = pd.to_datetime(Table["Data"], errors="coerce")
 
     # Filtrar os dados por data:
-    
-    start_date = st.date_input("Data de início", value=pd.to_datetime("2024-01-01"))
-    end_date = st.date_input("Data de término", value=pd.to_datetime("today"))
+    start_date = col3.date_input("Data de início", value=pd.to_datetime("2024-01-01"))
+    end_date = col3.date_input("Data de término", value=pd.to_datetime("today"))
 
     if start_date and end_date:
         Table = Table[

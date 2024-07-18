33107d243ec527268c124b96692f1bb64dd493f7
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-07-17 22:09:34 -0300

    Update logoff.py

--- logout/logoff.py
+++ logout/logoff.py
@@ -1,4 +1,16 @@
+""" Importando a biblioteca necessária """
+
 import streamlit as st
+
+"""
+
+- Objetivo da Função: A função `Logout` é usada para desconectar um usuário.
+- Funcionalidades: 
+  - Marca o status do usuário (`logged_in`) como `False`.
+  - Remove a identificação do usuário (`user_id`) definindo-a como `None`.
+  - Reinicia a aplicação para aplicar imediatamente essas mudanças.
+
+"""
 
 
 def Logout():

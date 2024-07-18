c37bfe3421059e86752000cb8da3ef9be758615c
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-07-17 22:04:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,38 @@
+""" Importando as bibliotecas necessárias """
+
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from inputs.form import FormDeem
-from streamlit_option_menu import option_menu
-from information.info import Table
-from login.login import Login
-from logout.logoff import Logout
+from inputs.Form import FormDeem
+from information.Info import Table
+from logout.Logoff import Logout
 from Edition.Edit import Edits
-
-# Inicializando o serviço do banco de dados Firebase:
-try:
-    firebase_admin.get_app()
-except ValueError as e:
-    cred = credentials.Certificate("credentials.json")
-    firebase_admin.initialize_app(cred)
-
-database = firestore.client()
-
-st.set_page_config(page_title="Controle de Deem")
+from Initializer.Initializers import (
+    InitializeFirebase,
+    SetupPage,
+    CheckLoginStatus,
+    HandleLogin,
+    DisplayMenu,
+)
 
 
-def app():
-
-    # Garante que sempre teremos um estado inicial de não logado:
-
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
-
-    # Verificamos se o usuário está logado:
+def main():
+    """Função principal para executar o aplicativo Streamlit"""
+    InitializeFirebase()
+    SetupPage()
+    CheckLoginStatus()
 
     if not st.session_state.logged_in:
-        user = Login()
-        if user:
-            st.session_state.logged_in = True
-            st.session_state.user_id = user
-            st.rerun()
+        HandleLogin()
     else:
-        with st.sidebar:
-            option = option_menu(
-                menu_title="Menu de navegação",
-                options=[
-                    "Formulário de Deem",
-                    "Visão Geral",
-                    "Editar Deem",
-                    "Sair",
-                ],
-                icons=[
-                    "journal-text",
-                    "table",
-                    "pencil-square",
-                    "door-closed",
-                ],
-                default_index=0,
-            )
-
+        option = DisplayMenu()
         if option == "Formulário de Deem":
             FormDeem()
-        if option == "Visão Geral":
+        elif option == "Visão Geral":
             Table()
-        if option == "Editar Deem":
+        elif option == "Editar Deem":
             Edits()
-        if option == "Sair":
+        elif option == "Sair":
             Logout()
 
 
 if __name__ == "__main__":
-    app()
+    main()
